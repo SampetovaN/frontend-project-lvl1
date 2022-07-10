57f7a56b64abfb25b5f116928e6e74a17b78b39f
Veronika Sampetova <81315475+sampetova@users.noreply.github.com>
2022-07-10 14:45:35 +0300

    add progression.js

---
+++ src/progression.js
@@ -0,0 +1,46 @@
+import game from '../src/index.js';
+import { randomNumber, randomInt } from '../src/util.js';
+
+const DESCRIPTION = 'What number is missing in the progression?';
+const EMPTY_NUMBER = '..';
+
+const makeProgression = (length, step) => {
+  const progression = [];
+  for (let i = 0; i <= length; i += 1) {
+    progression.push((i === 0 ? randomNumber() : progression[i - 1]) + step);
+  }
+  return progression;
+};
+
+const randomProgression = () => {
+  const randomProggressionLength = randomInt(5, 10);
+  const randomEmptyNumber = randomInt(0, randomProggressionLength - 1);
+  const progression = makeProgression(randomProggressionLength, randomNumber());
+  progression[randomEmptyNumber] = EMPTY_NUMBER;
+
+  return progression.join(' ');
+};
+
+const getValidResult = (progression) => {
+  let numbers = progression.split(' ').map((num) => num !== EMPTY_NUMBER ? Number(num) : num);
+  let foundNumber;
+  for (let i = 0; i <= numbers.length; i += 1) {
+    if (i === 0 && numbers[i] === EMPTY_NUMBER) {
+      foundNumber = numbers[i + 2] - (numbers[i + 2] - numbers[i + 1]);
+      break;
+    } else if (i === numbers.length && numbers[i] === EMPTY_NUMBER) {
+      const lastIndex = numbers.length;
+      foundNumber = numbers[lastIndex - 1] + (numbers[lastIndex - 1] - numbers[lastIndex - 2]);
+    } else if (numbers[i] === EMPTY_NUMBER) {
+      foundNumber = numbers[i - 1] + ((numbers[i + 1] - numbers[i - 1]) / 2);
+      break;
+    }
+  }
+  return foundNumber;
+};
+
+const progression = () => {
+  game(randomProgression, getValidResult, DESCRIPTION);
+};
+
+export default progression;
