415de4438d1d5d1cbaed12d199b817ec197ca3bf
Veronika Sampetova <81315475+sampetova@users.noreply.github.com>
2022-07-10 14:53:11 +0300

    add progression.js

--- bin/games/brain-progression.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-import greeting from '../../src/greeting.js';
-import setPrompt from '../../src/prompt.js';
-import progression from '../../src/progression.js';
-
-setPrompt();
-greeting();
-progression();
