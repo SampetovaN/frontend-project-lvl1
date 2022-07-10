6bfbb774fac8d00108f4ddb917a9370d75aaaac3
Veronika Sampetova <81315475+sampetova@users.noreply.github.com>
2022-07-10 14:54:47 +0300

    add progression.js

--- bin/brain-even.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-import even from '../src/even.js';
-import greeting from '../src/greeting.js';
-import setPrompt from '../src/prompt.js';
-
-setPrompt();
-greeting();
-even();
---
+++ bin/games/brain-progression.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+import greeting from '../../src/greeting.js';
+import setPrompt from '../../src/prompt.js';
+import progression from '../../src/progression.js';
+
+setPrompt();
+greeting();
+progression();
